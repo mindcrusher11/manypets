4e03a2f9084216bb21e92d62b62b81f6f2030952
Gaurhari <gaurharidass@gmail.com>
2022-12-06 19:33:18 +0000

    added constants and updated code

---
+++ src/main/scala/utils/QuantexxaConstants.scala
@@ -0,0 +1,20 @@
+package org.manypets.cam
+package utils
+
+/*
+ * Constants for Quantexxa tasks
+ *
+ * @author Gaurhari
+ *
+ *
+ * */
+object QuantexxaConstants {
+
+  val passengerIdColumn = "passengerId"
+  val fromDateColumn = "from"
+  val toDateColumn = "to"
+  val countriesColumn = "countries"
+  val longestRunColumn = "longestRun"
+  val countColumn = "count"
+
+}
